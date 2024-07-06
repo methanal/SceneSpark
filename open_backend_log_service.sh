67414b656e6f95b12054ede0bd08b85706106ec5
nathonxxxx <nathonxxxx@gmail.com>
2024-07-06 15:29:43 +0800

    fix

--- open_backend_log_service.sh
+++ open_backend_log_service.sh
@@ -6,4 +6,5 @@ export CONTAINER_ID=$(docker inspect --format="{{.Id}}" scenespark-dev-api-1)
 export LOG_PATH=/var/lib/docker/containers/${CONTAINER_ID}/${CONTAINER_ID}-json.log
 sudo chmod 666 ${LOG_PATH}
 
+sudo docker rm -f backendlog
 sudo docker run --name backendlog -p 0.0.0.0:11002:80 -v ${LOG_PATH}:/tmp/backend.log -v /home/luckybear/SceneSpark/pv-data/nginx/backendlog.dev.conf:/etc/nginx/conf.d/default.conf:ro --platform=linux/amd64 -d nginx
